3dab5e135b9fdd2605c406eb743477a038aef74a
hopecris31 <hope.crisafi@gmail.com>
2022-02-23 14:51:11 -0500

    asdfghjk

--- src/proj3/PokerHand.java
+++ src/proj3/PokerHand.java
@@ -6,7 +6,7 @@ public class PokerHand { //how to set up constructor as instructed
 
     int DEFAULT_HAND = 5;
     public ArrayList<Card> handContents;
-    public Card;
+
 
     /**
      * Constructor, initializes the hand
@@ -20,10 +20,35 @@ public class PokerHand { //how to set up constructor as instructed
     }
 
 
-    public Card addCard(){
+    /**
+     * adds a card to the hand
+     *
+     * checks how many cards in hand
+     * if 5 card, do nothing
+     * if less than 5 cards, add a card
+     *
+     * @param card, a card object
+     * @return
+     */
+    public Card addCard(Card card){
     }
 
-    public int getIthCard(int inxex){
+    /**
+     *return the card at specified index
+     *
+     * @param index the index of which card you want returned
+     * @return Card at specified index
+     */
+    public int getIthCard(int index){
+
+    }
+
+    public String toString(){
+        String handPrint = new String();
+        for(int i=0; i<= (handContents.size() - 1); i++){
+            handPrint += (handContents.get(i)).toString() + "\n";
+        }
+        return handPrint;
     }
 
 }
